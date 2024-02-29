a64c708467b010a73d96a29c12bd16966adb7137
Ramon Bijl <snc19917@gmail.com>
2024-02-29 12:30:14 +0100

    Add basic step motor code

--- Embedded-Stepper-Motor/Embedded-Stepper-Motor.ino
+++ Embedded-Stepper-Motor/Embedded-Stepper-Motor.ino
@@ -1,9 +1,7 @@
 void setup() {
-  // put your setup code here, to run once:
-  ///////test
+  stepMotorSetup();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  stepMotorRun();
 }
--- README.md
+++
@@ -1,2 +0,0 @@
-# Embedded-Stepper-Motor
- EmbeddedOpdracht
